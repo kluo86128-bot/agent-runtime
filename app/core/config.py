c3f4de5bdb0f5303610f98dacc26e5ea0346194d
lkw <3129287226@qq.com>
2026-08-05 17:59:51 +0800

    将重构的代码合并到主分支并且删除源分支中多余的文件

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Settings:
-    REDIS_HOST:str=os.getenv("REDIS_HOST","localhost")
-    REDIS_PORT:int=int(os.getenv("REDIS_PORT",6379))
-    REDIS_DB:int=int(os.getenv("REDIS_DB","0"))
-
-settings=Settings()
--- app/queue/task_queue.py
+++
@@ -1,9 +0,0 @@
-from rq import Queue
-
-from app.db.redis_client import redis_client
-
-task_queue=Queue(
-    name="agent-tasks",
-    connection=redis_client,
-    default_timeout=600
-)
